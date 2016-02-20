ff378b99e0337d7b1ddc584f55d9ff6d60bfd4ac
TemplarRei <boris.stoyanovv@gmail.com>
2016-02-20 12:28:37 +0200

    CreateCv initiated.

--- Source/Web/MV.CV.Web/Controllers/HomeController.cs
+++ Source/Web/MV.CV.Web/Controllers/HomeController.cs
@@ -6,18 +6,6 @@
     {
         public ActionResult Index()
         {
-            //var jokes = this.jokes.GetRandomJokes(3).To<JokeViewModel>().ToList();
-            //var categories =
-            //    this.Cache.Get(
-            //        "categories",
-            //        () => this.jokeCategories.GetAll().To<JokeCategoryViewModel>().ToList(),
-            //        30 * 60);
-            //var viewModel = new IndexViewModel
-            //{
-            //    Jokes = jokes,
-            //    Categories = categories
-            //};
-
             return this.View();
         }
     }
---
+++ Source/Web/MV.CV.Web/Controllers/MainCvController.cs
@@ -0,0 +1,18 @@
+﻿namespace MV.CV.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public class MainCvController : BaseController
+    {
+        [HttpGet]
+        [Authorize]
+        public ActionResult Create()
+        {
+            return this.View();
+        }
+    }
+}
